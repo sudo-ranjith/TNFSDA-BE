b4f6508e5486db3d93746f00751e09c9cf763ad1
Ranjith kumar <45558016+sudo-ranjith@users.noreply.github.com>
2021-06-12 20:04:07 +0530

    Update serializers.py
    Updated Rank Data types in register API

--- app/Register/serializers.py
+++ app/Register/serializers.py
@@ -9,7 +9,7 @@ register = api.model("register", {
     "username": fields.String(required=True, description="Enter username"),
     "email": fields.String(required=True, description="Enter email_address"),
     "mobile": fields.Integer(required=True, description="Enter phone_number"),
-    "rank": fields.Integer(required=True, description="Enter user RANK"),
+    "rank": fields.String(required=True, description="Enter user RANK"),
     "password": fields.String(required=True, description="Enter password"),
 })
 
