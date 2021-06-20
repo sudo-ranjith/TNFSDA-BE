d9b18b1142cc08db81545995a1ac97939a4a2690
beast <sudo.ranjith@gmail.com>
2021-06-20 12:33:12 +0530

    added few more parameters in register api

--- app/Register/serializers.py
+++ app/Register/serializers.py
@@ -6,10 +6,11 @@ from app import api
 
 
 register = api.model("register", {
-    "username": fields.String(required=True, description="Enter username"),
+    "first_name": fields.String(required=True, description="Enter first_name"),
+    "last_name": fields.String(required=True, description="Enter last_name"),
     "email": fields.String(required=True, description="Enter email_address"),
     "mobile": fields.Integer(required=True, description="Enter phone_number"),
+    "id_number": fields.String(required=True, description="Enter user id_number"),
     "rank": fields.String(required=True, description="Enter user RANK"),
     "password": fields.String(required=True, description="Enter password"),
 })
-
