f0f8e8b62abe66c22500e6d3c42b7d67fd8a72c5
beast <sudo.ranjith@gmail.com>
2021-06-21 18:48:14 +0530

    created approval apis

--- app/fire_call/view.py
+++ app/fire_call/view.py
@@ -102,3 +102,55 @@ class Login(Resource):
             return common_helpers.response('failed',
                                            app.config["FAILURE_MESSAGE_500"],
                                            more_info, [], 500)
+
+
+@fire_cal.route('approve')
+# @jwt_required
+class Login(Resource):
+    """
+         This class get form data
+         @return: success or failure message
+     """
+
+    @fire_cal.expect(fire_call_serializers.fire_call, validate=True)
+    @fire_cal.response(200, app.config["SUCCESS_MESSAGE_200"], fire_call_serializers.fire_call)
+    @fire_cal.response(301, app.config["FAILURE_MESSAGE_301"], common_serializers.response_api_model)
+    @fire_cal.response(400, app.config["FAILURE_MESSAGE_400"], common_serializers.response_api_model)
+    @fire_cal.response(401, app.config["FAILURE_MESSAGE_401"], common_serializers.response_api_model)
+    @fire_cal.response(403, app.config["FAILURE_MESSAGE_403"], common_serializers.response_api_model)
+    @fire_cal.response(404, app.config["FAILURE_MESSAGE_404"], common_serializers.response_api_model)
+    @fire_cal.response(409, app.config["FAILURE_MESSAGE_409"], common_serializers.response_api_model)
+    @fire_cal.response(422, app.config["FAILURE_MESSAGE_422"], common_serializers.response_api_model)
+    @fire_cal.response(500, app.config["FAILURE_MESSAGE_500"], common_serializers.response_api_model)
+    def put(self):
+        try:
+            if not (request.content_type == 'application/json'):
+                return common_helpers.response('failed',
+                                               app.config["FAILURE_MESSAGE_400"],
+                                               'Content type should be application/json',
+                                               [], 400)
+            post_data = request.get_json()
+            token = post_data.get("token")
+            current_user = get_jwt_identity()
+            # check user has valid access token
+
+            post_data['created_at'] = datetime.now().strftime('%Y%m%d%H%M%S%f')
+            post_data['created_by'] = current_user
+            id_number = post_data.get("id_number")
+            post_data['active'] = 1
+            user_item = fire_call_model.RegisterCurb()
+            user_item = user_item.find_modify({'id_number': id_number}, post_data)
+
+            more_info = "Successfully updated firecall data"
+            return common_helpers.response('success',
+                                           app.config["SUCCESS_MESSAGE_200"],
+                                           more_info,
+                                           [],
+                                           200,
+                                           post_data.get('token'))
+        except Exception as e:
+            e = f"{traceback.format_exc()}"
+            more_info = "Unable to Inserted data :Exception occurred - " + str(e)
+            return common_helpers.response('failed',
+                                           app.config["FAILURE_MESSAGE_500"],
+                                           more_info, [], 500)
--- app/rescue_call/view.py
+++ app/rescue_call/view.py
@@ -100,3 +100,56 @@ class Login(Resource):
             return common_helpers.response('failed',
                                            app.config["FAILURE_MESSAGE_500"],
                                            more_info, [], 500)
+
+
+@rescue_cal.route('approve')
+# @jwt_required
+class Login(Resource):
+    """
+         This class get form data
+         @return: success or failure message
+     """
+
+    @rescue_cal.expect(rescue_call_serializers.rescue_call, validate=True)
+    @rescue_cal.response(200, app.config["SUCCESS_MESSAGE_200"], rescue_call_serializers.rescue_call)
+    @rescue_cal.response(301, app.config["FAILURE_MESSAGE_301"], common_serializers.response_api_model)
+    @rescue_cal.response(400, app.config["FAILURE_MESSAGE_400"], common_serializers.response_api_model)
+    @rescue_cal.response(401, app.config["FAILURE_MESSAGE_401"], common_serializers.response_api_model)
+    @rescue_cal.response(403, app.config["FAILURE_MESSAGE_403"], common_serializers.response_api_model)
+    @rescue_cal.response(404, app.config["FAILURE_MESSAGE_404"], common_serializers.response_api_model)
+    @rescue_cal.response(409, app.config["FAILURE_MESSAGE_409"], common_serializers.response_api_model)
+    @rescue_cal.response(422, app.config["FAILURE_MESSAGE_422"], common_serializers.response_api_model)
+    @rescue_cal.response(500, app.config["FAILURE_MESSAGE_500"], common_serializers.response_api_model)
+    def put(self):
+        try:
+            if not (request.content_type == 'application/json'):
+                return common_helpers.response('failed',
+                                               app.config["FAILURE_MESSAGE_400"],
+                                               'Content type should be application/json',
+                                               [], 400)
+            post_data = request.get_json()
+            token = post_data.get("token")
+            current_user = get_jwt_identity()
+            # check user has valid access token
+
+            post_data['created_at'] = datetime.now().strftime('%Y%m%d%H%M%S%f')
+            post_data['created_by'] = current_user
+            id_number = post_data.get("id_number")
+            post_data['active'] = 1
+            user_item = rescue_call_model.RegisterCurb()
+            user_item = user_item.find_modify({'id_number': id_number}, post_data)
+
+            more_info = "Successfully updated rescue data"
+            return common_helpers.response('success',
+                                           app.config["SUCCESS_MESSAGE_200"],
+                                           more_info,
+                                           [],
+                                           200,
+                                           post_data.get('token'))
+        except Exception as e:
+            e = f"{traceback.format_exc()}"
+            more_info = "Unable to Inserted data :Exception occurred - " + str(e)
+            return common_helpers.response('failed',
+                                           app.config["FAILURE_MESSAGE_500"],
+                                           more_info, [], 500)
+
